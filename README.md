01690263bf1de334f7f4c5989df90fe039084d83
Sivana Alexa Hamer <sivanahamer@gmail.com>
2023-02-27 14:24:42 -0600

    Added initial docusaurus page

--- README.md
+++ README.md
@@ -10,10 +10,6 @@ Curso de [Diseño de software](https://www.ecci.ucr.ac.cr/cursos/ci-0136) de la 
 
 El diseño arquitectónico y detallado de software determina la calidad de los productos de software que se entregan. Es por esto que la industria del software ha venido sistematizando heurísticas de diseño por medio de lo que se ha llamado principios y patrones de diseño. En la actualidad, este cuerpo de conocimientos es muy amplio y relevante para cualquier ingeniero de software que deba construir software de calidad. El diseño y la construcción de frameworks para familias de productos de software requiere particularmente de la aplicación intensiva de este cuerpo de conocimientos por tratarse de un tipo de software adaptable a la construcción de aplicaciones de una familia con suficiente variabilidad de características como para ser sostenible en un nicho de mercado. Es por esto que el curso se enfoca en la aplicación de principios y patrones de diseño a la construcción de frameworks sin que por ello deba limitarse exclusivamente a este ámbito el uso de los principios y patrones estudiados.
 
-> [!NOTE]
-> El entorno virtual del sitio se encuentra en [Mediación virtual](https://mv1.mediacionvirtual.ucr.ac.cr/course/view.php?id=29989) donde se encuentran otros materiales y asignaciones del curso.
->
-
 ## Licencia
 
 This work is licensed under a `<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">`Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License `</a>`
---
+++ docusaurus.config.js
@@ -0,0 +1,96 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Diseño de Software',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://sivanahamer.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/software-design/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'sivanahamer', // Usually your GitHub org/user name.
+  projectName: 'software-design', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Diseño de software',
+        logo: {
+          alt: 'Software design Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Clases',
+          },
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()}, Sivana Hamer. This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        lightTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
